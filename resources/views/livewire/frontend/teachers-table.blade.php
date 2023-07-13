dad31a873510ced3a57e8fe5901086e4df4c9dda
Yahyobek <mywhite060608@gmail.com>
2023-07-14 00:10:35 +0500

    added some futures

--- resources/views/livewire/frontend/teachers-table.blade.php
+++ resources/views/livewire/frontend/teachers-table.blade.php
@@ -6,38 +6,45 @@
                     @foreach ($teachers as $teacher)
                         <div class="col-lg-3 col-md-3 col-sm-6 col-6 full-wdth">
                             <div class="teacher">
-                                <div class="teacher-img"><img style="width: 235px; height: 425px;" src="{{asset("images/$teacher->image")}}" alt="" class="w-100">
+                                <div class="teacher-img"><img style="width: 235px; height: 425px;"
+                                        src="{{ asset("images/$teacher->image") }}" alt="" class="w-100">
 
                                     <div class="teacher-info">
-                                    <h3><a href="#" title="">{{ $teacher->firstname }}
-                                            {{ $teacher->lastname }}</a></h3>
-                                    <span>{{ $teacher->category }}
-                                        O'qituvchisi</span>
-                                        @foreach ($teacher->degrees as $degree)
-                                        <span class="text-dark">
-                                            {{ App\Models\Degree::TYPES[$degree->type_id] }}
-                                            
-                                        </span>
+                                        <h3><a href="#" title="">{{ $teacher->firstname }}
+                                                {{ $teacher->lastname }}</a></h3>
+                                        <span>{{ $teacher->category }}
+                                            O'qituvchisi</span>
+                                        @if (empty($teacher->degrees))
+                                            <span class="text-dark">
+                                                Yangi toifa
+                                            </span>
+                                        @else
+                                            @foreach ($teacher->degrees as $degree)
+                                                <span class="text-dark">
+                                                    {{ App\Models\Degree::TYPES[$degree->type_id] }}
+
+                                                </span>
                                             @endforeach
+                                        @endif
+                                    </div>
                                 </div>
+                                <!--teacher end-->
                             </div>
-                            <!--teacher end-->
-                        </div>
                         </div>
                     @endforeach
+                </div>
+                <!--teachers end-->
             </div>
-            <!--teachers end-->
+            <!--teachers-section end-->
+            <div class="mdp-pagiation">
+                <nav aria-label="Page navigation example">
+                    <ul class="pagination">
+                        <li class="page-item"><a wire:click="viewMore" role="button" type="button"
+                                class="page-link">Yana</a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+            <!--pagination-end-->
         </div>
-        <!--teachers-section end-->
-        <div class="mdp-pagiation">
-            <nav aria-label="Page navigation example">
-                <ul class="pagination">
-                    <li class="page-item"><a wire:click="viewMore" role="button" type="button"
-                            class="page-link">Yana</a>
-                    </li>
-                </ul>
-            </nav>
-        </div>
-        <!--pagination-end-->
-    </div>
 </section>
