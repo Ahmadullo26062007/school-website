c7ffaa97891f85c1db3f57f71d706c55d691a4e1
Ahmadullo26062007 <113325761+Ahmadullo26062007@users.noreply.github.com>
2023-07-31 17:52:48 +0500

    fsewdsad

--- resources/views/frontend/home/index.blade.php
+++ resources/views/frontend/home/index.blade.php
@@ -52,7 +52,7 @@
                         @foreach ($classes as $class)
                             <div class="col-lg-3">
                                 <div class="classes-col wow fadeInUp" data-wow-duration="1000ms">
-                                    <div class="class-thumb"><img src="{{ asset('images/' . $class->image) }}"
+                                    <div class="class-thumb"><img src="{{ '../../images/'. $class->image }}"
                                                                   alt="" class="w-100">
 
                                     </div>
@@ -63,7 +63,7 @@
                                         <span>Xar kuni</span> <span></span>
                                         <div class="d-flex flex-wrap align-items-center">
                                             <div class="posted-by"><img style="width: 30px; height: 30px"
-                                                                        src="{{ asset('images/' . $class->teacher->image) }}"
+                                                                        src="{{'../../images/' . $class->teacher->image}}"
                                                                         alt="">
                                                 <a href="assets/images/resources/bg4.jpg.html#"
                                                    title="">{{ $class->teacher->firstname }}
