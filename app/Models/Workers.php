a61942d7593d5606ccf2171008028729dae1822a
Ahmadullo26062007 <ahmadullo0039@gmail.com>
2023-08-15 11:38:18 +0500

    dddw

---
+++ app/Models/Workers.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Workers extends Model
+{
+    use HasFactory;
+
+    protected $fillable=[
+        'fullname',
+        'role_id',
+        'school_id'
+    ];
+    public function role(){
+        return $this->belongsTo(Role::class);
+    }
+
+    public function schools()
+    {
+        return $this->belongsTo(About::class);
+    }
+}
